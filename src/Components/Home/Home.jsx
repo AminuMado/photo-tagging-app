2678497bb932a33d1b4d630526ba790fbbe69e75
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-14 16:22:30 +0100

    add level card component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,22 @@
 import Nav from "../Nav/Nav";
+import LevelCard from "../Level/LevelCard";
+import data from "../../data";
 const Home = () => {
-  return <Nav active="Home" />;
+  const levels = data.level.map((level) => (
+    <LevelCard name={level.name} image={level.image} />
+  ));
+  return (
+    <>
+      <Nav active="Home" />
+      <main>
+        <header>
+          <h1>Choose a Level</h1>
+        </header>
+        <section className="flex justify-evenly flex-wrap">{levels}</section>
+      </main>
+    </>
+  );
+
   /* My home component shoul have a these components
 NavBar Component
 Level Selecton Card component
