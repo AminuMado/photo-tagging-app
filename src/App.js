5a52e1a1f7b80f47ebe0114822880a3341606090
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-14 10:43:30 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Leaderboard from "./Components/LeaderBoard/LeaderBoard";
+// import { BrowserRouter } from "react-router-dom";
 function App() {
-  return <div> Hello App</div>;
+  return (
+    <div
+      className="
+    bg-slate-800"
+    >
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Game" element="" />
+          <Route path="/Leaderboard" element={<Leaderboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
