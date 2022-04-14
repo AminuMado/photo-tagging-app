4dd6dbe524697022bfffb11edaed2dc90e7cd909
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-14 17:50:58 +0100

    add back button

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,15 +1,35 @@
+import React, { useState } from "react";
 import Nav from "../Nav/Nav";
 import LevelCard from "../Level/LevelCard";
 import data from "../../data";
+import Overlay from "../Overlay/Overlay";
 const Home = () => {
+  const [active, setActive] = useState(false);
+  const toggleOverlay = () => {
+    setActive((prevActive) => !prevActive);
+  };
   const levels = data.level.map((level) => (
-    <LevelCard name={level.name} image={level.image} />
+    <LevelCard
+      name={level.name}
+      image={level.image}
+      key={level.name}
+      clicked={toggleOverlay}
+    />
   ));
   return (
     <>
+      <Overlay active={active} />
       <Nav active="Home" />
       <main>
-        <header>
+        {active && (
+          <button
+            className="text-neutral-400 hover:scale-110 duration-300 absolute z-30 right-5 top-5 font-bold text-3xl "
+            onClick={toggleOverlay}
+          >
+            <- Back
+          </button>
+        )}
+        <header className="font-bold text-5xl text-center m-6 p-3 ">
           <h1>Choose a Level</h1>
         </header>
         <section className="flex justify-evenly flex-wrap">{levels}</section>
