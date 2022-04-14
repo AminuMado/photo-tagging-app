639e26344c53a20e18e003f8c7d46ad8048a8bd1
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-14 15:41:47 +0100

    update styling

--- src/Components/Level/LevelCard.jsx
+++ src/Components/Level/LevelCard.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const LevelCard = (props) => {
   return (
     <div
-      className="flex flex-col w-96 h-96 min-w-[250px] items-center justify-center m-5 overflow-clip border-2 rounded-md 
-      border-gray-400 relative hover:scale-110 duration-300"
+      className="flex flex-col w-[600px] h-96 min-w-[250px] items-center justify-center m-3 overflow-clip border-2 rounded-md 
+      border-gray-400 relative hover:scale-105 duration-300"
       onClick={props.clicked}
     >
       <img src={props.image} alt={props.name} />
-      <h1 className="absolute z-10 top-0 text-2xl p-3 bg-black w-full text-center">
-        {props.Children}
+      <h1 className="absolute z-10 top-0 text-yellow-400 text-2xl p-3 bg-black w-full text-center">
+        {props.name}
       </h1>
     </div>
   );
