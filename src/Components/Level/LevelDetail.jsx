5373dd89c300a98159e5374d94b064450e8673ba
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-14 21:25:36 +0100

    breakpoint:working on template

--- src/Components/Level/LevelDetail.jsx
+++ src/Components/Level/LevelDetail.jsx
@@ -1,3 +1,23 @@
-const LevelDetail = () => {};
+import data from "../../data";
+
+const LevelDetail = () => {
+  return (
+    <div className="flex  text-white m-4">
+      <div className="w-1/2 h-[600px] overflow-clip">
+        <img src={data.level[2].image} alt={data.level[2].name}></img>
+      </div>
+      <div className="w-1/2">
+        <div>
+          <h1>Select Difficulty</h1>
+          <div>
+            <button>Easy</button>
+            <button>Hard</button>
+          </div>
+        </div>
+      </div>
+      <button>Start</button>
+    </div>
+  );
+};
 
 export default LevelDetail;
