caadee02a1de0eb60821e2a81c8e11cbf3c70d9c
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-15 21:02:01 +0100

    change opacity

--- src/Components/Overlay/Overlay.js
+++ src/Components/Overlay/Overlay.js
@@ -3,7 +3,7 @@ const Overlay = (props) => {
     <div
       className={`${
         props.active ? "translate-x-0" : "-translate-x-full"
-      } fixed h-screen w-screen bg-black opacity-80 pointer-events-all z-20 duration-700`}
+      } fixed h-screen w-screen bg-black opacity-95 pointer-events-all z-20 duration-700`}
     ></div>
   );
 };
