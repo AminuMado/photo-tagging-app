f02733a4be5cbb2f713530adaf25a268d240db2b
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-15 21:13:39 +0100

    add game component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import Game from "./Components/Game/Game";
 import Leaderboard from "./Components/LeaderBoard/LeaderBoard";
 // import { BrowserRouter } from "react-router-dom";
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Game" element="" />
+          <Route path="/Game" element={<Game />} />
           <Route path="/Leaderboard" element={<Leaderboard />} />
         </Routes>
       </BrowserRouter>
