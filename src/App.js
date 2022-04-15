821f55289d6e92f52486ee14f98f85a69b08f3f4
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-15 21:52:01 +0100

    add current game state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Game from "./Components/Game/Game";
 import Leaderboard from "./Components/LeaderBoard/LeaderBoard";
 // import { BrowserRouter } from "react-router-dom";
 function App() {
+  const [currentGame, setCurrentGame] = useState("");
   return (
     <div className=" font-Crosshatcher">
       <BrowserRouter basename="/">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Game" element={<Game />} />
+          <Route path="/" element={<Home setGame={setCurrentGame} />} />
+          <Route path="/Game" element={<Game currentGame={currentGame} />} />
           <Route path="/Leaderboard" element={<Leaderboard />} />
         </Routes>
       </BrowserRouter>
