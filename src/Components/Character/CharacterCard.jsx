5b783eb59ca3b1b2c03813f0367cb05c8200815e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-15 20:53:26 +0100

    remove hover effect and pointer

--- src/Components/Character/CharacterCard.jsx
+++ src/Components/Character/CharacterCard.jsx
@@ -1,7 +1,7 @@
 const CharacterCard = (props) => {
   return (
-    <div className="text-white hover:scale-110 duration-300 flex flex-col items-center justify-center w-52 m-4 p-4">
-      <div className="w-56 h-60 flex justify-center overflow-clip cursor-pointer">
+    <div className="text-white duration-300 flex flex-col items-center justify-center w-52 m-4 p-4">
+      <div className="w-56 h-60 flex justify-center overflow-clip">
         <img className="" src={props.image} alt={props.name} />
       </div>
       <div className="flex flex-col items-center justify-center font-Inconsolata text-lg ">
