f3ef2ea13aedad44ef1ab974a43e42d3c5c0afbe
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-15 11:24:24 +0100

    add easy and hard button styling

--- src/Components/Level/LevelDetail.jsx
+++ src/Components/Level/LevelDetail.jsx
@@ -9,9 +9,13 @@ const LevelDetail = () => {
       <div className="w-1/2">
         <div>
           <h1>Select Difficulty</h1>
-          <div>
-            <button>Easy</button>
-            <button>Hard</button>
+          <div className="flex gap-10 text-xl ">
+            <button className="w-24 bg-orange-500 p-3 m-3 rounded-lg hover:scale-105  hover:text-lime-500 hover:font-bold  active:scale-100">
+              Easy
+            </button>
+            <button className="w-24 bg-purple-600 p-3 m-3 rounded-lg hover:text-blue-400 hover:font-bold hover:scale-105 active:scale-100">
+              Hard
+            </button>
           </div>
         </div>
       </div>
