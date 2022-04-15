89a727248b3b0d3ee778e55bb34db979a69bfda1
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-15 20:58:10 +0100

    add focus state to difficulty button

--- src/Components/Level/LevelDetail.jsx
+++ src/Components/Level/LevelDetail.jsx
@@ -1,48 +1,66 @@
-import data from "../../data";
+import React from "react";
 import CharacterCard from "../Character/CharacterCard";
 
-const LevelDetail = () => {
-  function shuffle(array) {
-    return array.sort(() => Math.random() - 0.5).slice(2);
-  }
-  const FCharacters = data.level[0].mode[0].characters;
-  const Characters = shuffle(FCharacters);
-
-  const displayCharacters = Characters.map((character) => {
-    return (
-      <CharacterCard
-        name={character.name}
-        image={character.image}
-        title={character.title}
-        key={character.name}
-      />
-    );
-  });
-
+const LevelDetail = ({ level, active, easyMode, hardMode, characters }) => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="flex  text-white m-4">
-        <div className="w-1/2 h-[600px] overflow-clip">
-          <img src={data.level[2].image} alt={data.level[2].name}></img>
-        </div>
-        <div className="w-1/2">
-          <div className="flex flex-col justify-center items-center gap-2">
-            <h1 className="text-5xl">Select Difficulty</h1>
-            <div className="flex gap-10 text-xl ">
-              <button className=" bg-orange-500 w-24 p-3 m-3 rounded-lg hover:text-lime-500 hover:font-bold hover:scale-105 active:scale-100 ">
-                Easy
-              </button>
-              <button className=" bg-purple-600 w-24 p-3 m-3 rounded-lg hover:text-blue-400 hover:font-bold hover:scale-105 active:scale-100">
-                Hard
-              </button>
+    <div
+      className={`${
+        active ? "translate-x-0" : "-translate-x-full"
+      } fixed top-36 h-screen w-screen bg-black opacity-80 pointer-events-all z-30 duration-700`}
+    >
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex  text-white m-4">
+          <div className="w-1/2 h-[600px] overflow-clip">
+            <img src={level.image} alt={level.name}></img>
+          </div>
+          <div className="w-1/2">
+            <div className="flex flex-col justify-center items-center gap-2">
+              <h1 className="text-5xl">Select Difficulty</h1>
+              <div className="flex gap-10 text-xl ">
+                <button
+                  onClick={easyMode}
+                  className=" bg-orange-500 w-24 p-3 m-3 rounded-lg hover:text-lime-500 hover:font-bold hover:scale-105 active:scale-100 focus:border-2 border-dashed"
+                >
+                  Easy
+                </button>
+                <button
+                  onClick={hardMode}
+                  className=" bg-purple-600 w-24 p-3 m-3 rounded-lg hover:text-blue-400 hover:font-bold hover:scale-105 active:scale-100 focus:border-2 border-dashed"
+                >
+                  Hard
+                </button>
+              </div>
+              {characters && (
+                <div className="flex items-center">
+                  {characters.map((character) => {
+                    return (
+                      <CharacterCard
+                        name={character.name}
+                        image={character.image}
+                        title={character.title}
+                        key={character.name}
+                      />
+                    );
+                  })}
+                </div>
+              )}
             </div>
-            <div className="flex items-center">{displayCharacters}</div>
           </div>
         </div>
+        <button
+          disabled={characters}
+          className={
+            characters
+              ? "bg-yellow-400 text-5xl w-60 p-5 m-3 rounded-lg hover:font-bold hover:scale-105 active:scale-100 opacity-100 transition-opacity duration-500"
+              : "opacity-0"
+          }
+          onClick={() => {
+            console.log("start butn cliked");
+          }}
+        >
+          Start
+        </button>
       </div>
-      <button className="bg-yellow-400 text-5xl w-60 p-5 m-3 rounded-lg hover:font-bold hover:scale-105 active:scale-100">
-        Start
-      </button>
     </div>
   );
 };
