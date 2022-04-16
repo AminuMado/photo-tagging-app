43979b63401259177225710170bb64e8b9fb860b
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-16 10:34:04 +0100

    add props destructuring

--- src/Components/Character/CharacterCard.jsx
+++ src/Components/Character/CharacterCard.jsx
@@ -1,12 +1,12 @@
-const CharacterCard = (props) => {
+const CharacterCard = ({ name, title, image }) => {
   return (
     <div className="text-white duration-300 flex flex-col items-center justify-center w-52 m-4 p-4">
       <div className="w-56 h-60 flex justify-center overflow-clip">
-        <img className="" src={props.image} alt={props.name} />
+        <img src={image} alt={name} />
       </div>
       <div className="flex flex-col items-center justify-center font-Inconsolata text-lg ">
-        <h1>{props.name}</h1>
-        <h2 className="text-xl italic">{props.title}</h2>
+        <h1>{name}</h1>
+        <h2 className="text-xl italic">{title}</h2>
       </div>
     </div>
   );
