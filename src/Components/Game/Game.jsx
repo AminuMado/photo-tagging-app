469b3f759b1aa0f6ff539a674d0d7e83cb6e9a61
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-16 04:54:48 +0100

    add menu button styling

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -8,8 +8,8 @@ const Game = (props) => {
       <div
         className={
           active
-            ? "w-96 h-full bg-red-600 absolute transition-all duration-300 left-7 scale-95 rounded-md flex items-center p-4 "
-            : "w-32 h-full -left-1/4 bg-red-600 absolute transition-all duration-300"
+            ? "w-96 h-full bg-slate-800 absolute transition-all duration-300 left-7 scale-95 rounded-md flex items-center p-4 "
+            : "w-32 h-full -left-1/4 bg-slate-800 absolute transition-all duration-300"
         }
       >
         <div>sidebar</div>
@@ -17,12 +17,14 @@ const Game = (props) => {
       <main
         className={
           active
-            ? "scale-x-95 scale-y-95 ml-96 opacity-40 rounded-md h-full w-full grid place-items-center transition-all duration-300 bg-slate-400"
+            ? "scale-x-95 scale-y-95 ml-96 opacity-20 pointer-events-none rounded-md h-full w-full grid place-items-center transition-all duration-300 bg-slate-400"
             : "h-full w-full grid place-items-center transition-all duration-300"
         }
       >
         <div>
-          Main Content
+          <p className="w-20 h-20 rounded-full bg-blue-400 flex items-center justify-center text-4xl">
+            4
+          </p>
           <button onClick={() => setActive((prev) => !prev)}>Click Me</button>
         </div>
       </main>
