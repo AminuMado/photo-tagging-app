be8363194fae8289b503b00a2cb9b3fe8439cb1e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-16 10:02:37 +0100

    add timer logic

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -1,2 +1,31 @@
-const Timer = () => {};
+import React, { useState, useEffect } from "react";
+
+function Timer({ isGameOver }) {
+  const [timer, setTimer] = useState(0);
+  const formatTime = (time) => {
+    const getSeconds = `0${Math.round(time % 60)}`.slice(-2);
+    const minutes = `${Math.floor(time / 60)}`;
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+    const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
+
+    return `${getHours}:${getMinutes}:${getSeconds}`;
+  };
+  useEffect(() => {
+    let interval = null;
+    if (!isGameOver) {
+      interval = setInterval(() => {
+        setTimer((timer) => timer + 1);
+      }, 1000);
+    } else if (isGameOver) {
+      clearInterval(interval);
+      setTimer(0);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isGameOver]);
+
+  return <>{formatTime(timer)}</>;
+}
+
 export default Timer;
