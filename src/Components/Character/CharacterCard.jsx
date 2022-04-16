7d8891a440411409df985363554e5a32a39ac614
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-16 11:24:25 +0100

    update styling

--- src/Components/Character/CharacterCard.jsx
+++ src/Components/Character/CharacterCard.jsx
@@ -1,10 +1,22 @@
-const CharacterCard = ({ name, title, image }) => {
+const CharacterCard = ({ name, title, image, size }) => {
   return (
-    <div className="text-white duration-300 flex flex-col items-center justify-center w-52 m-4 p-4">
-      <div className="w-56 h-60 flex justify-center overflow-clip">
+    <div
+      className={
+        size === "lg"
+          ? "text-white flex flex-col items-center justify-center w-52 m-4 p-4"
+          : "text-white flex justify-around w-full"
+      }
+    >
+      <div
+        className={
+          size === "lg"
+            ? "w-56 h-60 flex justify-center overflow-clip"
+            : "w-40 max-w-[160px] min-w-[160px] m-2"
+        }
+      >
         <img src={image} alt={name} />
       </div>
-      <div className="flex flex-col items-center justify-center font-Inconsolata text-lg ">
+      <div className="flex flex-col items-center justify-center font-Inconsolata text-lg text-center">
         <h1>{name}</h1>
         <h2 className="text-xl italic">{title}</h2>
       </div>
