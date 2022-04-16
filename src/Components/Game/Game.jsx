604abcb7e693234326df82f73fb42145ba20f762
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-16 04:44:01 +0100

    add page layout

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -1,6 +1,33 @@
+import { useState } from "react";
+
 const Game = (props) => {
   console.log(props.currentGame);
-  return <h1>Hello Game</h1>;
+  const [active, setActive] = useState(false);
+  return (
+    <div className="flex h-screen w-screen  text-white">
+      <div
+        className={
+          active
+            ? "w-96 h-full bg-red-600 absolute transition-all duration-300 left-7 scale-95 rounded-md flex items-center p-4 "
+            : "w-32 h-full -left-1/4 bg-red-600 absolute transition-all duration-300"
+        }
+      >
+        <div>sidebar</div>
+      </div>
+      <main
+        className={
+          active
+            ? "scale-x-95 scale-y-95 ml-96 opacity-40 rounded-md h-full w-full grid place-items-center transition-all duration-300 bg-slate-400"
+            : "h-full w-full grid place-items-center transition-all duration-300"
+        }
+      >
+        <div>
+          Main Content
+          <button onClick={() => setActive((prev) => !prev)}>Click Me</button>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Game;
