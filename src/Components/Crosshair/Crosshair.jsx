3cf6e3c1f9b9e5c74d45b66f539f3fe78ebc18d1
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-17 18:35:24 +0100

    style changes: opacity

--- src/Components/Crosshair/Crosshair.jsx
+++ src/Components/Crosshair/Crosshair.jsx
@@ -1,6 +1,9 @@
-const Crosshair = () => {
+const Crosshair = ({ coordinates }) => {
   return (
-    <div className="rounded-full bg-white opacity-40  border-black border-4 border-dashed w-20 h-20 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
+    <div
+      className="rounded-full absolute bg-white opacity-60  border-black border-4 border-dashed w-20 h-20 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center"
+      style={coordinates}
+    >
       <span className="w-2 h-2 bg-red-600 rounded-full"></span>
     </div>
   );
