577aa37b26c33b8a98fcb1fe8d183e3acc938953
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-17 21:59:42 +0100

    add console log

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -106,6 +106,7 @@ const Game = ({ currentGame }) => {
           onClick={(e) => {
             imageClick(e);
             const scaledCoords = getCoordinates(e);
+            console.log(scaledCoords);
           }}
         />
         <button
