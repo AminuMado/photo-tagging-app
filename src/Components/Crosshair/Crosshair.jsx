03a747a059c0ea9b14e92b9bf6849731c9728924
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-17 18:21:33 +0100

    initial commit

---
+++ src/Components/Crosshair/Crosshair.jsx
@@ -0,0 +1,8 @@
+const Crosshair = () => {
+  return (
+    <div className="rounded-full bg-white opacity-40  border-black border-4 border-dashed w-20 h-20 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
+      <span className="w-2 h-2 bg-red-600 rounded-full"></span>
+    </div>
+  );
+};
+export default Crosshair;
