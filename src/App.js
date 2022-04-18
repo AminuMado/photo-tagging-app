87542d1e1652d63dcbf3d57f417424909dbcdc8e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-18 15:44:15 +0100

    remove duplicate import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Game from "./Components/Game/Game";
 import Leaderboard from "./Components/LeaderBoard/LeaderBoard";
-// import { BrowserRouter } from "react-router-dom";
 function App() {
   const [currentGame, setCurrentGame] = useState("");
   return (
