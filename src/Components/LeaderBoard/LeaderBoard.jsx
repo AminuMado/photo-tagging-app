d33b46604d89b577cd4dab244576ea94dee76b34
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-20 10:29:57 +0100

    add table

--- src/Components/LeaderBoard/LeaderBoard.jsx
+++ src/Components/LeaderBoard/LeaderBoard.jsx
@@ -59,8 +59,47 @@ const Leaderboard = () => {
           </button>
         </div>
       </div>
+      <table>
+        <tr>
+          <th>Place</th>
+          <th>Name</th>
+          <th>Time(seconds)</th>
+          <th>Date</th>
+        </tr>
+      </table>
     </>
   );
 };
 
 export default Leaderboard;
+
+// // Example of a data array that
+// // you might receive from an API
+// const data = [
+//   { name: "Anom", age: 19, gender: "Male" },
+//   { name: "Megha", age: 19, gender: "Female" },
+//   { name: "Subham", age: 25, gender: "Male"},
+// ]
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <table>
+//         <tr>
+//           <th>Name</th>
+//           <th>Age</th>
+//           <th>Gender</th>
+//         </tr>
+//         {data.map((val, key) => {
+//           return (
+//             <tr key={key}>
+//               <td>{val.name}</td>
+//               <td>{val.age}</td>
+//               <td>{val.gender}</td>
+//             </tr>
+//           )
+//         })}
+//       </table>
+//     </div>
+//   );
+// }
