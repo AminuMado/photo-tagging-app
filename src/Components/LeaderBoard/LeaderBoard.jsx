cf7d06126755f1442e80a9fd004e3834531af243
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-21 21:56:49 +0100

    refactred code

--- src/Components/LeaderBoard/LeaderBoard.jsx
+++ src/Components/LeaderBoard/LeaderBoard.jsx
@@ -1,12 +1,12 @@
 import Nav from "../Nav/Nav";
 import React, { useEffect, useState } from "react";
 
-const Leaderboard = (props) => {
+const Leaderboard = ({ currentGame }) => {
   // so i have a name but i want it to represent something else
   // if my state doesnt exist i wanna use the component as it is currently
   // my defualt state is just to ensure that it shows the current page when youre done with a game
-  const [currentLevel, setCurrentLevel] = useState(props.currentGame.levelName);
-  const [difficulty, setDifficulty] = useState(props.currentGame.difficulty);
+  const [currentLevel, setCurrentLevel] = useState(currentGame.levelName);
+  const [difficulty, setDifficulty] = useState(currentGame.difficulty);
 
   const levelList = [
     { name: "Cyberpunk City", color: "bg-zinc-300" },
