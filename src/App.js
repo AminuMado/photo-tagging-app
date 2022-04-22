a83e1ab6ab75f3bb4d6080537f93da75b44e071e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-22 21:31:45 +0100

    add firebase config

--- src/App.js
+++ src/App.js
@@ -25,3 +25,14 @@ function App() {
 }
 
 export default App;
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbl-rwQRQktbr8eQqH7LnsgQL05zzQoF0",
+  authDomain: "find-em-all.firebaseapp.com",
+  projectId: "find-em-all",
+  storageBucket: "find-em-all.appspot.com",
+  messagingSenderId: "381554495292",
+  appId: "1:381554495292:web:689944b96f65a5dafb9839",
+  measurementId: "G-23B1QTNEX7",
+};
